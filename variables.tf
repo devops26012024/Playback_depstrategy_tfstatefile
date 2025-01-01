124c7a5d8c0325eb2e267e8d63334b02d2b92a27
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-01-01 22:55:03 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "version_name" {
+  default = "v1.0.0"
+}
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "aws_ami" {
+  default = "ami-053b12d3152c0cc71"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "pemfile" {
+  default = "guru"
+}
