1b604d0ce2bbd60be896cac29736aa25e5d3d4f9
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-01-01 22:55:54 +0530

    Create root_main.tf

---
+++ root_main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH traffic"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EC2SecurityGroup"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "ec2" {
+  ami           = var.aws_ami
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  tags = {
+    Name    = "Feb2nd"
+    Version = var.version_name
+  }
+}
+
+# Output the version
+output "deployed_version" {
+  description = "The deployed version of the EC2 instance"
+  value       = var.version_name
+}
+
+# Output the instance public IP
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
